014311068fbb61cf6b89cfbd1a978254c07f39f5
jikkajikka <69613539+jikkajikka888@users.noreply.github.com>
2020-10-25 10:57:17 +0900

    devise導入、バリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Include both letters and numbers" }
+  validates :nickname, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters" }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" }
+  validates :birthday, presence: true
+
+end
